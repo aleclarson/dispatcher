e3c8e08b2d2b7579ac8283848addbc9a1a15fc79
Alec Larson <alec.stanford.larson@gmail.com>
2014-10-17 20:28:21 -0700

    Added DispatchGroup test

--- DispatcherTests/DispatcherTests.swift
+++ DispatcherTests/DispatcherTests.swift
@@ -5,31 +5,35 @@ import Dispatcher
 
 class DispatcherTests: XCTestCase {
 
-  override func setUp () {
-    super.setUp()
-  }
-
-  override func tearDown () {
-    super.tearDown()
-  }
-
-  func testIsCurrent () {
+  func testDispatchQueueIsCurrent () {
     XCTAssert(gcd.main.isCurrent)
     XCTAssert(!gcd.isCurrent)
     gcd.sync { XCTAssert(gcd.isCurrent) }
   }
 
-  func testCurrent () {
+  func testDispatcherCurrent () {
     XCTAssert(gcd.main === gcd.current)
     XCTAssert(gcd !== gcd.current)
     gcd.sync { XCTAssert(gcd === gcd.current) }
   }
 
-  func testSerial () {
+  func testSerialDispatchQueue () {
     var calls = 0
     let queue = gcd.serial()
     queue.async { XCTAssert(++calls == 1) }
     queue.async { XCTAssert(++calls == 2) }
     queue.sync { XCTAssert(++calls == 3) }
+  }
+
+  func testDispatchGroup () {
+    let expectation = expectationWithDescription(nil)
+
+    let group = DispatchGroup(1)
+    ++group
+    gcd.async { --group }
+    group.done(expectation.fulfill)
+    --group
+
+    waitForExpectationsWithTimeout(2) { XCTAssertNil($0) }
   }
 }
