8b41abdad8917c35583d3d0a7e1e4b3d4b01d650
aleclarsoniv <alec.stanford.larson@gmail.com>
2014-11-16 22:25:53 -0800

    47

--- DispatchTimer.swift
+++ DispatchTimer.swift
@@ -25,15 +25,14 @@ public class DispatchTimer {
       return
     }
 
-    callbackQueue = gcd.current
-    queue = gcd.high
-//    if !gcd.main.isCurrent { dispatch_set_target_queue(queue.dispatch_queue, gcd.current.dispatch_queue) }
-    timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, queue.dispatch_queue)
+    _callbackQueue = gcd.current
+    _queue = gcd.high
+    _timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, _queue.dispatch_queue)
     let delay_ns = delay * Seconds(NSEC_PER_SEC)
     let time = dispatch_time(DISPATCH_TIME_NOW, Int64(delay_ns))
-    dispatch_source_set_timer(timer, time, UInt64(delay_ns), UInt64(tolerance * Seconds(NSEC_PER_SEC)))
-    dispatch_source_set_event_handler(timer) { [weak self] in let _ = self?.fire() }
-    dispatch_resume(timer)
+    dispatch_source_set_timer(_timer, time, UInt64(delay_ns), UInt64(tolerance * Seconds(NSEC_PER_SEC)))
+    dispatch_source_set_event_handler(_timer) { [weak self] in let _ = self?.fire() }
+    dispatch_resume(_timer)
   }
 
 
@@ -49,52 +48,64 @@ public class DispatchTimer {
   // MARK: Instance methods
 
   public func repeat (_ times: UInt! = nil) {
-    isRepeating = true
-    repeatsLeft = times != nil ? Int(times) : -1
+    _shouldRepeat = true
+    _remainingRepeats = times != nil ? Int(times) : -1
   }
 
   public func autorelease () {
-    isAutoReleased = true
+    if _invalidated { return }
+    _releasesItself = true
     autoReleasedTimers[ObjectIdentifier(self)] = self
   }
   
   public func fire () {
-    if OSAtomicAnd32OrigBarrier(1, &invalidated) == 1 { return }
-    println("Firing Timer.")
-    callbackQueue.sync(callback)
-    if isRepeating && repeatsLeft > 0 { repeatsLeft-- }
-    if !isRepeating || repeatsLeft == 0 { stop() }
+    if _invalidated { return }
+    _callbackQueue.sync(callback)
+    if _shouldRepeat && _remainingRepeats > 0 { _remainingRepeats-- }
+    if !_shouldRepeat || _remainingRepeats == 0 { stop() }
   }
   
   public func stop () {
-    if OSAtomicTestAndSetBarrier(7, &invalidated) { return }
-    println("Stopping Timer.")
-    queue.sync(dispatch_source_cancel(timer))
-    if isAutoReleased { autoReleasedTimers[ObjectIdentifier(self)] = nil }
+    if _invalidated { return }
+    _invalidated = true
+    _queue.sync(dispatch_source_cancel(_timer))
+    if _releasesItself { autoReleasedTimers[ObjectIdentifier(self)] = nil }
   }
 
 
 
   // MARK: Internal
 
-  let timer: dispatch_source_t!
+  let _timer: dispatch_source_t!
+  let _queue: DispatchQueue!
+  var _callbackQueue: DispatchQueue!
 
-  let queue: DispatchQueue!
+  var _releasesItself = false
 
-  var callbackQueue: DispatchQueue!
+  var _shouldRepeat = false
+  var _remainingRepeats = 0
 
-  var invalidated: UInt32 = 0
+  var __invalidated = false
+  var _invalidated: Bool {
+    get {
+      return lock(self) { self.__invalidated }
+    }
+    set {
+      lock(self) { self.__invalidated = newValue }
+    }
+  }
 
-  var isAutoReleased = false
-
-  var isRepeating = false
-
-  var repeatsLeft = 0
 
   deinit {
-    if !isAutoReleased { stop() }
-    println("Timer.deinit")
+    if !_releasesItself { stop() }
   }
 }
 
 var autoReleasedTimers = [ObjectIdentifier:Timer]()
+
+func lock <T> (obj: AnyObject, block: Void -> T) -> T {
+  objc_sync_enter(obj)
+  let value = block()
+  objc_sync_exit(obj)
+  return value
+}
