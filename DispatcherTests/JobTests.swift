28386947159f06eb102517c3f26be84c5d06bb86
aleclarsoniv <alec.stanford.larson@gmail.com>
2014-12-09 08:52:56 -0800

    156

--- DispatcherTests/JobTests.swift
+++ DispatcherTests/JobTests.swift
@@ -12,7 +12,10 @@ class JobTests : XCTestCase {
     var calls = 0
 
     JobVoid.async { _, done in
-      let _ = Timer(0.1) { XCTAssert(++calls == 2); done() }
+      let _ = Timer(0.5) {
+        XCTAssert(++calls == 2)
+        done()
+      }
     }.perform()
 
     XCTAssert(++calls == 1)
