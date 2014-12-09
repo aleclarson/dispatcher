ba2e1f71d19c39b4470e0ac0ce995098a9b5cbee
aleclarsoniv <alec.stanford.larson@gmail.com>
2014-12-09 08:39:03 -0800

    149

--- DispatcherTests/JobTests.swift
+++ DispatcherTests/JobTests.swift
@@ -7,10 +7,17 @@ class JobTests : XCTestCase {
 
   func testPerform () {
     let e = expectationWithDescription(nil)
+    var calls = 0
 
     Job.async {
       (_: Void, done: Void -> Void) in
-      Timer(0.5, done)
-    }.
+      let _ = Timer(0.1) { XCTAssert(++calls == 2); done() }
+    }.async(Queue.high) {
+      (_: Void, done: Void -> Void) in
+      let _ = Timer(0.1) { XCTAssert(++calls == 3); done() }
+    }
+
+    XCTAssert(++calls == 1)
+    waitForExpectationsWithTimeout(0.5, handler: nil)
   }
 }
