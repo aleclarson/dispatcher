89185b0d04cc776f2c9d36b6e911dc1fcc4d32c9
aleclarsoniv <alec.stanford.larson@gmail.com>
2014-12-08 16:28:24 -0800

    142

--- Timer.swift
+++ Timer.swift
@@ -6,30 +6,20 @@ public typealias Seconds = CGFloat
 
 /// You must use the `weak` keyword when creating a Timer property anywhere.
 public class Timer {
-  
-  public convenience init (_ delay: Seconds, _ callback: Void -> Void) {
-    self.init(delay, 0, callback)
-  }
 
-  public init (_ delay: Seconds, _ tolerance: Seconds, _ callback: Void -> Void) {
-    self.callback = callback
-    self.tolerance = tolerance
+  public init (_ delay: Seconds, _ callback: Void -> Void) {
 
-    if delay == 0 {
+    if delay <= 0 {
       callback()
       return
     }
 
-//    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(Seconds(NSEC_PER_SEC) * delay)), timerQueue.core) {
-//      [weak self] in let _ = self?.fire(true)
-//    }
+    _self = self
+    _callback = callback
 
-    _source = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, timerQueue.core)
-    dispatch_source_set_timer(_source, dispatch_time(DISPATCH_TIME_NOW, 0), UInt64(delay * Seconds(NSEC_PER_SEC)), UInt64(tolerance * Seconds(NSEC_PER_SEC)))
-    dispatch_source_set_event_handler(_source) { [weak self] in let _ = self?.fire(true) }
-    dispatch_resume(_source)
-
-    activeTimers[ObjectIdentifier(self)] = self
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(Seconds(NSEC_PER_SEC) * delay)), (Queue.current ?? Queue.main).core) {
+      [weak self] in let _ = self?.fire()
+    }
   }
 
 
@@ -37,10 +27,6 @@ public class Timer {
   // MARK: Read-only
 
   public var isActive: Bool { return _isActive.value }
-
-  public let tolerance: Seconds
-  
-  public let callback: Void -> Void
 
 
 
@@ -51,10 +37,10 @@ public class Timer {
     _remainingRepeats = times != nil ? Int(times) : -1
   }
   
-  public func fire (_ asynchronous: Bool = false) {
+  public func fire () {
     if !isActive { return }
     if _shouldRepeat && _remainingRepeats > 0 { _remainingRepeats-- }
-    (asynchronous ? _callingDispatcher.async : _callingDispatcher.sync)(callback)
+    _callback()
     if !_shouldRepeat || _remainingRepeats == 0 { _stop() }
   }
 
@@ -65,12 +51,12 @@ public class Timer {
 
 
   // MARK: Private
+  
+  private let _callback: (Void -> Void)!
 
-  private let _source: dispatch_source_t!
+  private var _self: Timer!
 
   private var _isActive = Lock(true, serial: true)
-
-  private let _callingDispatcher = Dispatcher.current
 
   private var _shouldRepeat = false
 
@@ -78,11 +64,6 @@ public class Timer {
 
   private func _stop () {
     _isActive.value = false
-    dispatch_source_cancel(_source)
-    activeTimers[ObjectIdentifier(self)] = nil
+    _self = nil
   }
 }
-
-let timerQueue = Queue.high
-
-var activeTimers = [ObjectIdentifier:Timer]()
