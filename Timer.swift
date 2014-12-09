8fb902c052fe24d8332b5111bcf5625c92aca89b
aleclarsoniv <alec.stanford.larson@gmail.com>
2014-12-08 16:19:29 -0800

    141

--- Timer.swift
+++ Timer.swift
@@ -20,14 +20,14 @@ public class Timer {
       return
     }
 
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(Seconds(NSEC_PER_SEC) * delay)), timerQueue.core) {
-      [weak self] in let _ = self?.fire(true)
-    }
+//    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(Seconds(NSEC_PER_SEC) * delay)), timerQueue.core) {
+//      [weak self] in let _ = self?.fire(true)
+//    }
 
-//    _source = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, timerQueue.core)
-//    dispatch_source_set_timer(_source, dispatch_time(DISPATCH_TIME_NOW, 0), UInt64(delay * Seconds(NSEC_PER_SEC)), UInt64(tolerance * Seconds(NSEC_PER_SEC)))
-//    dispatch_source_set_event_handler(_source) { [weak self] in let _ = self?.fire(true) }
-//    dispatch_resume(_source)
+    _source = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, timerQueue.core)
+    dispatch_source_set_timer(_source, dispatch_time(DISPATCH_TIME_NOW, 0), UInt64(delay * Seconds(NSEC_PER_SEC)), UInt64(tolerance * Seconds(NSEC_PER_SEC)))
+    dispatch_source_set_event_handler(_source) { [weak self] in let _ = self?.fire(true) }
+    dispatch_resume(_source)
 
     activeTimers[ObjectIdentifier(self)] = self
   }
@@ -78,7 +78,7 @@ public class Timer {
 
   private func _stop () {
     _isActive.value = false
-//    dispatch_source_cancel(_source)
+    dispatch_source_cancel(_source)
     activeTimers[ObjectIdentifier(self)] = nil
   }
 }
